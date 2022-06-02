0803128820860ef5332aece7faf9bb576776e858
jooy3410 <jooy3410@naver.com>
2022-06-02 23:45:29 +0900

    데이터베이스 세팅중

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const router = express.Router();
 const path = require('path');
+const mysql = require("mysql");
 
 app.set('port', process.env.PORT || 3000)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//database setting
 
 //routes setup
 var user = require('./routes/user.js');
