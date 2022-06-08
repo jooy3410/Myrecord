d0fec1f2ddcff2c603fb721edef4e5a0bec38a24
jooy3410 <jooy3410@naver.com>
2022-06-08 11:50:46 +0900

    회원가입 기능 진행중

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  port : '3306',
+  user: 'root',
+  password: 'abcd1234',
+  database: 'TEST_DB'
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
@@ -11,7 +19,14 @@ app.get('/join', (req, res) => {
 })
 
 app.post('/join_result', (req,res) =>{
+  const email = req.body.email;
+  const user_name = req.body.user_name;
+  const user_pw = req.body.user_pw;
+
+  connection.query(
+    "INSERT INTO USER_INFO(USER_EMAIL, USER_NAME, USER_PW) VALUES ('email', 'user_name', 'user_pw');"
+  )
   
-  res.redirect()
+  res.send()
 })
 module.exports = app;
